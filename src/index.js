9f64630d1c3e41d308609443393867458965b4cd
Gibson Ainge <gainge@gmail.com>
2021-02-16 15:39:19 -0800

    implement directory selection data flow

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
+const {ipcRenderer} = require('electron')
 const { BrowserWindow } = require('electron').remote
 const path = require('path');
+
+var selectedPath = null;
+
+// Grab a directory?
+const selectDirButton = document.getElementById('select-dir-button');
+
+selectDirButton.addEventListener('click', (e) => {
+  ipcRenderer.send('open-file-dialog')
+})
+
+ipcRenderer.on('selected-directory', (event, path) => {
+  selectedPath = path;
+  console.log(`user selected: [${path}]`);
+  document.getElementById('directory-path').innerHTML = `${path}`
+})
+
 
 // Aight, let's try opening this sucker up
 const openReaderButton = document.getElementById('open-reader-button');
