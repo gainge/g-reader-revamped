af5fd998947b55e8a2b121c6d074d7370c3997ac
Gibson Ainge <gainge@gmail.com>
2021-02-18 20:34:30 -0800

    bind basic hotkeys

--- src/reader.js
+++ src/reader.js
@@ -2,6 +2,7 @@ const remote = require('electron').remote
 const { ipcRenderer } = require('electron')
 const path = require('path')
 const fs = require('fs')
+const Mousetrap = require('mousetrap');
 
 const DISPLAY_SPREAD = 0;
 const DISPLAY_SINGLE = 1;
@@ -19,16 +20,31 @@ const page2 = document.getElementById('page-2');
 const closeButton = document.getElementById('close-reader-button');
 
 closeButton.addEventListener('click', (e) => {
-  var window = remote.getCurrentWindow();
-  window.close();
+  closeWindow();
 });
 
-function showPrevPage() {
+function closeWindow() {
+  let window = remote.getCurrentWindow();
+  window.close();
+}
 
+function addShortcuts() {
+  Mousetrap.bind('left', () => {showNextPage()});
+  Mousetrap.bind('right', () => {showPrevPage()});
+  Mousetrap.bind('esc', () => {closeWindow()});
+  Mousetrap.bind('m', () => {console.log('Pressed M! (I think?)')})
+}
+
+function getPageStep() {
+  return displayMode === DISPLAY_SPREAD ? 2 : 1;
+}
+
+function showPrevPage() {
+  changePage(currentPage - getPageStep())
 }
 
 function showNextPage() {
-
+  changePage(currentPage + getPageStep())
 }
 
 // Pre-condition that images have been populated
@@ -51,7 +67,6 @@ function changePage(pageNum) {
   }
 }
 
-
 ipcRenderer.on('reader-path', (e, imageDir) => {
   console.log(`Reader window received ${imageDir}`);
   console.log(typeof(imageDir))
@@ -61,5 +76,6 @@ ipcRenderer.on('reader-path', (e, imageDir) => {
     // Create our array of full image paths
     images = files.map((file) => path.join(imageDir, file))
     changePage(0); // Display the content!
+    addShortcuts();
   })
 });
